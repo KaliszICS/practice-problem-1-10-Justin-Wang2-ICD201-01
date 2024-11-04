7fc75faa8659f72d4c237c258b396fda0330568b
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-04 09:35:00 -0500

    Update PP1_10.py

--- PP1_10.py
+++ PP1_10.py
@@ -1,33 +1,41 @@
-
+  import math
 
 def q1(): 
   num = input("input a number: ")
-  num = sqrt(num)
-  print num
+  num = int(num)
+  num = math.sqrt(num)
+  print(num)
 
 def q2(): 
   num = input("input a number: ")
-  num = sqrt(num)
   num = int(num)
-  print num
+  num = math.sqrt(num)
+  print(num)
 
 def q3(): 
-num = input("input a number: ")
-num = floor(num)
-print num
-
+  num = input("input a number: ")
+  num = float(num)
+  num = math.floor(num)
+  print(num)
+  
 def q4(): 
-num = input("input a number: ")
-num = ceil(num)
-print num
+  num = input("input a number: ")
+  num = float(num)
+  num = math.ceil(num)
+  print(num)
+
 
 def q5(): 
-num = input("input a number: ")
-num1 = input("input another number: ")
-num2 = num1 * num
-num3 = num2 / 2
-num = floor(num3)
-print num
+  num = input("input a number: ")
+  num1 = input("input another number: ")
+  num = int(num)
+  num1 = int(num1)
+  num2 = num1 * num
+  num2 = int(num2)
+  num3 = num2 / 2
+  num3 = int(num3)
+  num = math.floor(num3)
+  print(num)
 
 
 
